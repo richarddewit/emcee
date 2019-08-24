a3cfd9b2506d09ed8cec825e6e7ee516079a9c24
Richard de Wit <henk.exe@gmail.com>
2019-08-25 01:08:11 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "sudo curl -L https://raw.githubusercontent.com/richarddewit/emcee/master/emcee -o /usr/local/bin/emcee"
+sudo curl -L https://raw.githubusercontent.com/richarddewit/emcee/master/emcee -o /usr/local/bin/emcee
+echo "sudo chmod +x /usr/local/bin/emcee"
+sudo chmod +x /usr/local/bin/emcee
